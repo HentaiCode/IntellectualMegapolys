9f5bb1c6096e265c1172e928c433df9584538295
dmtnm <dmtnmn@gmail.com>
2024-03-30 11:15:24 +0300

    Исправление ошибок

--- geterrorscount.py
+++ geterrorscount.py
@@ -1,21 +1,28 @@
-with open('db/game.txt', encoding='utf-8') as f:
-    data = [[j for j in i.split('$')] for i in f.readlines()]
+def geterrcount(games):
+    errs = {}
 
-errors = {}
+    for game in data:
+        if game[0] not in errors:
+            errs[game[0]] = 1
+        else:
+            errs[game[0]] += 1
 
-for game in data:
-    if game[0] not in errors:
-        errors[game[0]] = 1
-    else:
-        errors[game[0]] += 1
+    return errs
 
-for i in range(len(data)):
-    data[i][-1] = data[i][-1].strip()
-    data[i].append(str(errors[data[i][0]]) + '\n')
 
-data.sort(key=lambda x: x[-1])
+if __name__ == '__main__':
+    with open('db/game.txt', encoding='utf-8') as f:
+        data = [[j for j in i.split('$')] for i in f.readlines()]
 
-f = open('db/game_counter.csv', 'w', encoding='utf-8')
+    errors = geterrcount(data)
 
-f.writelines(['$'.join(i) for i in data])
-f.close()
+    for i in range(len(data)):
+        data[i][-1] = data[i][-1].strip()
+        data[i].append(str(errors[data[i][0]]) + '\n')
+
+    data.sort(key=lambda x: x[-1])
+
+    f = open('db/game_counter.csv', 'w', encoding='utf-8')
+
+    f.writelines(['$'.join(i) for i in data])
+    f.close()
